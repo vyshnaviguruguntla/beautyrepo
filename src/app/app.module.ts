bd528b141a88fb5cc18d3d578cfd9b6b74bf6bcd
vyshnaviguruguntla <39789395+vyshnaviguruguntla@users.noreply.github.com>
2023-04-19 18:21:08 +0530

    api changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -41,6 +41,7 @@ import { SignupComponent } from './shop/signup/signup.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     //SlickCarouselModule
   ],
--- src/app/product-single/product-single.component.ts
+++ src/app/product-single/product-single.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-single',
@@ -7,9 +9,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductSingleComponent implements OnInit {
 
-  constructor() { }
+  productList:any;
+  likedProducts:any;
+  searchList: any;
 
+  constructor(private http: HttpClient) { }
+  
   ngOnInit(): void {
+    this.displayProducts();
+    this.fetchLikedProducts();
   }
 
+  fetchProducts(){
+    const headers:any = [];
+    headers['Content-Type'] =  'application/json';
+    const url = "https://sample-poc-8a5f.vercel.app/api/getProducts"
+    const options =  {headers: new HttpHeaders(headers)}
+    return this.http.get(url, options);
+  }
+
+  fetchYouMayAlsoLikeService() {
+    const headers:any = [];
+    headers['Content-Type'] =  'application/json';
+    const url = "https://sample-poc-8a5f.vercel.app/api/getLikedProducts"
+    const options =  {headers: new HttpHeaders(headers)}
+    return this.http.get(url, options);
+  }
+
+  fetchLikedProducts(){
+    this.fetchYouMayAlsoLikeService().subscribe((res:any)=>{
+      this.likedProducts = res['Liked Hair Products'];
+      console.log("Liked productsList:",this.likedProducts);
+    },(err:any)=>{
+      console.log("error:",err);
+    })
+  }
+
+  displayProducts(){
+    this.fetchProducts().subscribe((res:any)=>{
+      this.productList = res['Hair Products'];
+      console.log("productsList:",this.productList);
+    },(err:any)=>{
+      console.log("error:",err);
+    })
+  }
+
+  searchProductsService(text:any){
+    const headers:any = [];
+    headers['Content-Type'] =  'application/json';
+    const url = "https://sample-poc-8a5f.vercel.app/api/search?searchKey="+ text;
+    const options =  {headers: new HttpHeaders(headers)}
+    return this.http.get(url, options);
+  }
+
+  searchProducts(text:any){
+    this.searchProductsService(text).subscribe((res:any)=>{
+      this.searchList = res['Hair Products'];
+      console.log("productsList:",this.productList);
+    },(err:any)=>{
+      console.log("error:",err);
+    })
+  }
 }
